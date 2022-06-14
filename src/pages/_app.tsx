73cf60943956516082c71ad1c93dcbdd0f1e6bd8
Jun-T <64676197+Jun-T-git@users.noreply.github.com>
2022-06-14 12:16:38 +0900

    Initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import "~/styles/style.css";
+import { AppProps } from "next/app";
+import { ReactElement } from "react";
+
+const MyApp = ({ Component, pageProps }: AppProps): ReactElement => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
