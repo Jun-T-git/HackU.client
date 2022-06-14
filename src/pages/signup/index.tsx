621b6330453e6cc80bcd97415e780fed3f5ee8a1
Jun-T-git <jun0303t@icloud.com>
2022-06-14 19:07:17 +0900

    mainタグの移動

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import SelectField from "~/components/field/selectField";
+import { prefectures } from "~/libs/constants/prefectures";
 
 const Index: React.VFC = () => {
   return (
     <>
-      <main>
-        <h1 className="text-xl font-bold">サインアップページ</h1>
-      </main>
+      <h1 className="text-xl font-bold">サインアップページ</h1>
+      <form className="mx-auto mt-5 max-w-3xl px-3">
+        <SelectField
+          fieldId="prefecture"
+          label="住んでいる都道府県"
+          options={prefectures.map((prefecture) => {
+            return { name: prefecture.name, value: prefecture.id };
+          })}
+        />
+      </form>
     </>
   );
 };
