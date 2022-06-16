9f643f73ea62a90e5c19452902e1eb0b51a70ed1
staku3 <csta19808@g.nihon-u.ac.jp>
2022-06-17 01:36:09 +0900

    #6
      サインインページの見た目を作成しました
      現状は何も入力しなくてもボタン押下のみで画面遷移をします

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,9 +1,46 @@
 import React from "react";
+import Router from "next/router"
+import Link from "next/link";
+import Button from "~/components/button/button";
+import TextField from "~/components/field/textField";
+
+function clickToIndex () {
+  Router.push("/")
+  return
+}
 
 const Index: React.VFC = () => {
   return (
     <>
       <h1 className="text-xl font-bold">サインインページ</h1>
+      <form className="mx-auto mt-5 max-w-3xl px-3">
+        <TextField
+          fieldId="email"
+          label="メールアドレス"
+        />
+        <TextField
+          fieldId="password"
+          label="パスワード"
+        />
+      </form>
+      <ul className="flex flex-col gap-2 text-center">
+        <li>
+          <Button
+            className="my-5"
+            onClick={() => {
+              alert("success!");
+              clickToIndex()
+            }}
+          >
+            Signin
+          </Button>
+        </li>
+        <li>
+          <Link href="/signup">
+            <a>アカウントをお持ちでない方はこちら</a>
+          </Link>
+        </li>
+      </ul>
     </>
   );
 };
