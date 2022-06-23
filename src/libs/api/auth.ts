ed4252a28cb8f4f2adf5a9a70766a46ef50d5722
Jun-T-git <jun0303t@icloud.com>
2022-06-23 19:06:44 +0900

    サインイン・サインアップ処理

--- src/libs/api/auth.ts
+++ src/libs/api/auth.ts
@@ -1,6 +1,35 @@
-/* auth関連 */
+import { axios, handleError } from "./config";
 
-export const signUp = async () => {
-  // ユーザー登録
-  return;
+export type SignUpParams = {
+  userId: string;
+  userName: string;
+  prefectureId: number;
+};
+
+export const signUp = async (params: SignUpParams) => {
+  const res = await axios
+    .post("/api/signup", params)
+    .catch((e) => {
+      throw Error(e);
+    })
+    .then(handleError)
+    .then((res) => res.json());
+  console.log(res);
+  return res;
+};
+
+export type SignInParams = {
+  userId: string;
+};
+
+export const signIn = async (params: SignInParams) => {
+  const res = await axios
+    .post("/api/signin", params)
+    .catch((e) => {
+      throw Error(e);
+    })
+    .then(handleError)
+    .then((res) => res.json());
+  console.log(res);
+  return res;
 };
