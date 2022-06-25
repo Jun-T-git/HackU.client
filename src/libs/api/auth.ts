76cd4fea214bd1b7cacaf8a58f56b9c8f5c8a3c2
Jun-T-git <jun0303t@icloud.com>
2022-06-25 15:21:07 +0900

    サインアウト処理追加

--- src/libs/api/auth.ts
+++ src/libs/api/auth.ts
@@ -1,3 +1,5 @@
+import { useResetRecoilState } from "recoil";
+import { userState } from "../recoil/user";
 import { axios } from "./config";
 
 export type SignUpParams = {
@@ -18,4 +20,11 @@ export type SignInParams = {
 export const signIn = async (params: SignInParams) => {
   const res = await axios.post("/api/signin", JSON.stringify(params));
   return res.data;
+};
+
+// 本来はapiに関わる処理であるためこのファイルに記載
+export const signOut = async () => {
+  const resetUser = useResetRecoilState(userState);
+  resetUser();
+  return;
 };
